0d82543d4d3a2f1ecf194730d465bf411ae86924
kemberlyt <kturcios@tywls.org>
2018-08-02 15:40:27 -0400

    fixed buttons

--- something.js
+++ something.js
@@ -1,12 +1,14 @@
 
+
 $(document).ready(function(){
 
 
+   
 
-    function changeBackgroundColor(dot){
+    
+   function changeBackgroundColor(dot){
     $("body").css("background-color",dot);
     }
-    
 
     $("#pink").click(function(){
     changeBackgroundColor("pink");
@@ -42,7 +44,17 @@ $(document).ready(function(){
 
     $("#white").click(function(){
     changeFont("Righteous");
+    });
 
+    function resetEverything(font,color,bgcolor){
+    $("h1").css("font-family",font)
+    $("h1").text("Robot")
+    $("img").attr("src","http://cliparting.com/wp-content/uploads/2016/09/Robot-clipart-free-clipart-images.png")
+    $("body").css("background-color",bgcolor)
+    $("h1").css("color", color);
+    }
 
+    $("#reset").click(function(){
+        resetEverything("Press Start 2P", "white", "black");
     });
 });
