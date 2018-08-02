7dc4c784b82ac3d5f2b0111a02c17a7f6951e791
kemberlyt <kturcios@tywls.org>
2018-08-02 11:03:41 -0400

    idk

--- something.js
+++ something.js
@@ -1,44 +1,48 @@
 
-function changeBackgroundColor(dot){
+$(document).ready(function(){
+
+
+
+    function changeBackgroundColor(dot){
     $("body").css("background-color",dot);
+    }
+    
+
+    $("#pink").click(function(){
+    changeBackgroundColor("pink");
+    });
+
+    function changeImage(){
+    $("img").attr("src","http://moziru.com/images/robot-clipart-female-1.png");
+    }
+
+    $("#blue").click(function(){
+    changeImage();
+    });
+
+    function changeFontColor(color){
+    $("h1").css("color",color);
+    }
+
+    $("#red").click(function(){
+    changeFontColor("aqua");
+    });
+
+    function changeText(){
+    $("h1").text("Kemberly's Robot");
+    }
+
+    $("#purple").click(function(){
+    changeText();
+    });
+
+    function changeFont(font){
+    $("h1").css("font-family",font);
+    }
+
+    $("#white").click(function(){
+    changeFont("Righteous");
 
 
-}
-
-$("#pink").click(function() {
-changeBackgroundColor("pink");
-
-});
-
-function changeImage(){
-$("img").attr("src","http://moziru.com/images/robot-clipart-female-1.png");
-}
-
-$("#blue").click(function(){
-changeImage();
-});
-
-function changeFontColor(color){
- $("h1").css("color",color);
-
-}
-
-
-$("#red").click(function(){
-changeFontColor("violet");
-});
-
-function changeText(){
-$("h1").text("Kemberly <3 ");
-}
-
-$("#Purple").click(function(){
-changeText();
-});
-
-function changeFont(font){
- $("h1").css("font-family",font);
-}
-$("#white").click(function(){
-changeFont("League Script");
+    });
 });
